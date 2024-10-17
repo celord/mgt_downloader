e1611034d0d62b6be2d424362a7124f82b489cce
cesar <celord@gmail.com>
2024-10-17 09:55:04 -0600

    fixing install issue

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mgt_downloader',
-    version='0.1.0',
+    version='0.1.1',
     description='A script to download torrents using Real Debrid',
     author='Cesar Garcia',
     author_email='celord@gmail.com',
